a645a3586c5fecf89722b7752b37a44bcb8f1a29
TakatokiOshima <ki13sd6p@gmail.com>
2022-06-11 03:05:25 +0900

    pref: 同一ディレクトリを毎回作成しているので最初に作成

--- main.go
+++ main.go
@@ -21,12 +21,13 @@ import (
 const DATE_FORMAT string = "20060102150405"
 
 func main() {
-	executeTime := time.Now().Format(DATE_FORMAT)
+	dir := fmt.Sprintf("./resource/%s", time.Now().Format(DATE_FORMAT))
+	os.Mkdir(dir, 0777)
 	for i := 1; i <= 25; i++ {
 		url, title := scrape()
 		fmt.Println(title)
 
-		extendImageBottom(download(url, fmt.Sprintf("%03d", i), executeTime))
+		extendImageBottom(download(url, fmt.Sprintf("%03d", i), dir))
 		time.Sleep(time.Second * 1)
 	}
 }
@@ -53,15 +54,11 @@ func scrape() (string, string) {
 	return "https:" + src, text[5:]
 }
 
-func download(url string, number string, executeTime string) (path string) {
-
+func download(url string, number string, dir string) (path string) {
 	res, _ := http.Get(url)
 	defer res.Body.Close()
 
-	dir := fmt.Sprintf("./resource/%s", executeTime)
-	os.Mkdir(dir, 0777)
-
-	path = fmt.Sprintf("%s/%s.jpg", dir, number)
+	path = fmt.Sprintf("%s/%s.png", dir, number)
 
 	file, _ := os.Create(path)
 	defer file.Close()
@@ -81,7 +78,7 @@ func extendImageBottom(path string) {
 	defer outputFile.Close()
 
 	m := image.NewRGBA(image.Rect(0, 0, img.Bounds().Dx(), img.Bounds().Dy()+100))
-	c := color.RGBA{255, 255, 255, 255}
+	c := color.RGBA{0, 0, 255, 255}
 
 	draw.Draw(m, m.Bounds(), &image.Uniform{c}, image.ZP, draw.Src)
 
